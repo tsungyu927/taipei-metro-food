d7a06d3858f84fb542a8239ad024cad607293577
tsungyu927 <tsungyu927@gmail.com>
2022-10-29 22:35:35 +0800

    feat: resize the svg according to window size

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,15 +1,53 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Station from 'components/station'
 import stationData from 'data/station.json'
 
 import { MapDataProps } from 'interface/I_Map'
 
 const Map = () => {
-  const scale = 1
+  // the aspect of 【svg】
+  const aspect = useMemo(() => stationData.size.width / stationData.size.height, [])
+  // the size of 【svg】
+  const [size, setSize] = useState({ width: 960, height: 1286 })
+  // the scale of 【element】 in svg
+  const [scale, setScale] = useState(1)
+
+  const handleResize = () => {
+    const { innerWidth: windowWidth, innerHeight: windowHeight } = window
+    if (windowWidth < windowHeight) {
+      // Portrait (mobile)
+      setSize({ width: windowWidth, height: windowWidth / aspect })
+      return
+    }
+    // Landscape (computer)
+    setSize({ width: windowHeight * aspect, height: windowHeight })
+  }
+
+  useEffect(() => {
+    // initial the svg size
+    handleResize()
+  }, [])
+
+  useEffect(() => {
+    // listen whether window size has changed
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    // calculate the scale
+    if (size.width / aspect < size.height) {
+      // use width to calculate the scale
+      setScale(size.width / stationData.size.width)
+      return
+    }
+    // use height to calculate the scale
+    setScale(size.height / stationData.size.height)
+  }, [size])
 
   const svgStyle = {
-    width: 960,
-    height: 1286,
+    width: size.width,
+    height: size.height,
     background: '#E7E7E7'
   }
 
@@ -26,7 +64,7 @@ const Map = () => {
   , [scale])
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center overflow-auto">
       <svg style={svgStyle}>
         <g>{stations}</g>
       </svg>
